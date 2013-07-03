361150a704d486cf854c3c1e7007920b80021e4d
Jin Hu <bixuehujin@gmail.com>
2013-07-03 14:58:09 +0800

    Add cookie storage support.

--- test/ApiTestCase.php
+++ test/ApiTestCase.php
@@ -8,13 +8,29 @@
 
 use Guzzle\Http\Client;
 use Guzzle\Http\Message\Response;
+use Guzzle\Plugin\Cookie\CookiePlugin;
+use Guzzle\Plugin\Cookie\CookieJar\ArrayCookieJar;
+use Guzzle\Plugin\Cookie\CookieJar\FileCookieJar;
 
 Yii::setPathOfAlias('Guzzle', __DIR__ . '/../vendors/guzzle/src/Guzzle');
 Yii::setPathOfAlias('Symfony', __DIR__ . '/../vendors/event-dispatcher/Symfony');
 
 class ApiTestCase extends CDbTestCase {
 	
+	/**
+	 * The base url of all api requests.
+	 * 
+	 * @var string
+	 */
 	public $baseUrl;
+	
+	/**
+	 * Specify the cookie storage used by HttpClient, array or file, default to null for do not use cookie.
+	 * 
+	 * @var string
+	 */
+	public $cookieStorage;
+	
 	/**
 	 * @var Guzzle\Http\Client
 	 */
@@ -27,6 +43,17 @@ class ApiTestCase extends CDbTestCase {
 	public function setUp() {
 		parent::setUp();
 		$this->client = new Client($this->baseUrl);
+		if ($this->cookieStorage === null) return;
+		
+		if ($this->cookieStorage == 'array') {
+			$cookiePlugin = new CookiePlugin(new ArrayCookieJar());
+		}else if ($this->cookieStorage == 'file'){ 
+			touch('/tmp/api.cookie');
+			$cookiePlugin = new CookiePlugin(new FileCookieJar('/tmp/api.cookie'));
+		}else {
+			throw new CException(Yii::t('yii', 'Unsupportted cookie storage type {type}.', array('{type}' => $this->cookieStorage)));
+		}
+		$this->client->addSubscriber($cookiePlugin);
 	}
 	
 	/**
@@ -40,7 +67,11 @@ class ApiTestCase extends CDbTestCase {
 			$uri2 = array_shift($uri);
 			$uri = $uri2 . '?' . http_build_query($uri, '', '&');
 		}
-		return $this->baseUrl . '/' . $uri;
+		if (preg_match('/^https?:\/\//', $uri)) {
+			return $uri;
+		}else {
+			return $this->baseUrl . '/' . $uri;
+		}
 	}
 	
 	/**
@@ -79,7 +110,7 @@ class ApiTestCase extends CDbTestCase {
 	}
 	
 	/**
-	 * Perform a PUT request.
+	 * Perform a DELETE request.
 	 *
 	 * @param mixed $uri
 	 * @param mixed $body
