df5bd643d580247ec9333243735359abf05c4255
Jin Hu <bixuehujin@gmail.com>
2013-10-02 23:00:45 +0800

    Added built in web server support in tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,5 +9,21 @@ defined('YII_DEBUG') or define('YII_DEBUG', true);
 
 require_once($yiit);
 
+date_default_timezone_set('UTC');
+
+$command = sprintf('php -S %s:%d -t %s >/dev/null 2>&1 & echo $!', WEB_SERVER_HOST, WEB_SERVER_PORT, WEB_SERVER_DOCROOT);
+
+$output = array();
+exec($command, $output);
+$pid = (int) $output[0];
+
+echo sprintf('%s - Web server started on %s:%d with PID %d', date('r'), WEB_SERVER_HOST, WEB_SERVER_PORT, $pid) . PHP_EOL;
+
+// Kill the web server when the process ends
+register_shutdown_function(function() use ($pid) {
+	echo sprintf('%s - Killing process with ID %d', date('r'), $pid) . PHP_EOL;
+	exec('kill ' . $pid);
+});
+
 Yii::createWebApplication($config);
 //Yii::createConsoleApplication($config);
