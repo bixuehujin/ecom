2679c14de4042d278ffb41d31dacf1fea8f889e7
Jin Hu <bixuehujin@gmail.com>
2013-10-11 11:52:59 +0800

    remove settings component from global namespace

--- models/sql/setting.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS `setting`;
-
-CREATE TABLE `setting` (
-  `name` varchar(255) NOT NULL,
-  `value` text NOT NULL,
-  UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- models/sql/system.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS `system`;
-
-CREATE TABLE `system` (
-  `name` varchar(255) NOT NULL,
-  `value` text NOT NULL,
-  UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
